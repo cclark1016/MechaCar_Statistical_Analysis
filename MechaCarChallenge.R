16cdf23a0c2f1c39fa5c0dc53b4c066e48826c98
Cristobal <chrisclark1016@gmail.com>
2020-10-27 20:44:19 -0400

    1st Deliverable

---
+++ MechaCarChallenge.R
@@ -0,0 +1,4 @@
+mecha_df <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=mecha_df))
+
